8393cd47172a5701948747675b722fc073b613ba
alex-ncsr <alex.nujnii@gmail.com>
2022-10-27 20:45:15 +0300

    Modified save and get method

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -10,26 +10,21 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        for (int i = 0; i <= size; i++) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
         size = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                size++;
-                break;
-            }
-        }
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        for (Resume searchingResume : storage) {
-            if (searchingResume != null && searchingResume.uuid.equals(uuid)) {
-                return searchingResume;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
             }
         }
         return null;
@@ -38,8 +33,8 @@ public class ArrayStorage {
     void delete(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                storage[i] = storage[size-1];
-                storage[size-1] = null;
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
                 size--;
                 break;
             }
