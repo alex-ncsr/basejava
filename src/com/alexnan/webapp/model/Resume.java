0cd6ba5212d36c96e08e9da626cb848df007159b
alex-ncsr <alex.nujnii@gmail.com>
2022-10-31 20:48:34 +0200

    Add getter and setter for uuid in Resume

--- src/com/alexnan/webapp/model/Resume.java
+++ src/com/alexnan/webapp/model/Resume.java
@@ -8,6 +8,13 @@ public class Resume {
     // Unique identifier
     private String uuid;
 
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
 
     @Override
     public String toString() {
