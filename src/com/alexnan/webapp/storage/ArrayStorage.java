a2884e5358f8363b09b8a341a467fb9b93031a86
alex-ncsr <alex.nujnii@gmail.com>
2022-11-03 20:14:44 +0200

    Add interface

--- src/com/alexnan/webapp/storage/ArrayStorage.java
+++ src/com/alexnan/webapp/storage/ArrayStorage.java
@@ -7,20 +7,18 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
+public class ArrayStorage implements Storage{
 
     protected static final int STORAGE_LIMIT = 1000;
     protected final Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size = 0;
 
     public void clear() {
-
-        Arrays.fill(storage, null);
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
     public void save(Resume r) {
-
         if (STORAGE_LIMIT <= size) {
             System.out.println("ERROR: not enough space in storage");
         } else if (findIndex(r.getUuid()) == -1) {
@@ -33,20 +31,18 @@ public class ArrayStorage {
     }
 
     public Resume get(String uuid) {
-
-        if (findIndex(uuid) != -1) {
-            return storage[findIndex(uuid)];
+        int index = findIndex(uuid);
+        if (index != -1) {
+            return storage[index];
         }
         return null;
     }
 
     public void update(Resume resume) {
-
         storage[findIndex(resume.getUuid())] = resume;
     }
 
     public void delete(String uuid) {
-
         if (findIndex(uuid) != -1) {
             storage[findIndex(uuid)] = storage[size - 1];
             storage[size - 1] = null;
@@ -59,16 +55,14 @@ public class ArrayStorage {
      */
     public Resume[] getAll() {
 
-        return Arrays.copyOf(storage, size);
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     public int size() {
-
         return size;
     }
 
-    public int findIndex(String uuid) {
-
+    private int findIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
