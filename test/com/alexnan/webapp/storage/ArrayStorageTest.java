eafaa0bce50fb72090ac729354371196fc038b58
alex-ncsr <alex.nujnii@gmail.com>
2022-11-27 22:05:13 +0200

    Add test package with classes

---
+++ test/com/alexnan/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,8 @@
+package com.alexnan.webapp.storage;
+
+public class ArrayStorageTest extends AbstractArrayStorageTest {
+
+    public ArrayStorageTest() {
+        super(new ArrayStorage());
+    }
+}
---
+++ test/com/alexnan/webapp/storage/SortedArrayStorageTest.java
@@ -0,0 +1,8 @@
+package com.alexnan.webapp.storage;
+
+public class SortedArrayStorageTest extends AbstractArrayStorageTest {
+
+    public SortedArrayStorageTest() {
+        super(new SortedArrayStorage());
+    }
+}
