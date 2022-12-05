61176a49546cf57a81c22089a451fa91584fc53c
alex-ncsr <alex.nujnii@gmail.com>
2022-12-05 22:57:13 +0200

    Add ListStorage

---
+++ src/com/alexnan/webapp/storage/ListStorage.java
@@ -0,0 +1,61 @@
+package com.alexnan.webapp.storage;
+
+import com.alexnan.webapp.model.Resume;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListStorage extends AbstractStorage {
+    private final List<Resume> resumeList = new ArrayList<>();
+
+    @Override
+    protected Integer getSearchKey(String uuid) {
+        for (int i = 0; i < resumeList.size(); i++) {
+            if (resumeList.get(i).getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected boolean isExist(Object searchKey) {
+        return searchKey != null;
+    }
+
+    @Override
+    protected void doSave(Resume r, Object searchKey) {
+        resumeList.add((Integer) searchKey, r);
+    }
+
+    @Override
+    protected Resume doGet(Object searchKey) {
+        return resumeList.get((Integer) searchKey);
+    }
+
+    @Override
+    protected void doUpdate(Resume r, Object searchKey) {
+        resumeList.set((Integer) searchKey, r);
+    }
+
+    @Override
+    protected void doDelete(Object searchKey) {
+        resumeList.remove(((Integer) searchKey).intValue());
+    }
+
+
+    @Override
+    public void clear() {
+        resumeList.clear();
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return resumeList.toArray(new Resume[0]);
+    }
+
+    @Override
+    public int size() {
+        return resumeList.size();
+    }
+}
